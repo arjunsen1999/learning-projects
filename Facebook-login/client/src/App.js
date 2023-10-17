0af0cc35b490fb280f736c43b91c00a2fcf140a3
arjunsen1999 <arjunsen717@gmail.com>
2023-10-17 12:57:33 +0530

    update

--- Facebook-login/client/src/App.js
+++ Facebook-login/client/src/App.js
@@ -1,31 +1,38 @@
-import { useLogin } from "react-facebook";
+import { LoginButton, useLogin } from "react-facebook";
 import "./App.css";
 
 
 function App() {
-  const { login, status, isLoading, error} = useLogin();
-  console.log("status", status);
-  
-  async function handleLogin() {
-    try {
-      const response = await login({
-        scope: 'email',
-      });
+  // const { login, status, isLoading, error} = useLogin();
+  // console.log("status", status);
 
-      console.log("response.status", response.status);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
+  // async function handleLogin() {
+  //   try {
+  //     const response = await login({
+  //       scope: 'email',
+  //     });
+
+  //     console.log("response.status", response.status);
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
+  // }
+  const handleResponse = (data) => {
+    console.log(data);
+    // Handle the Facebook login response here
+  };
   return (
     <div className="App">
-      <button
+      {/* <button
         className="bg-blue-900 py-2 px-4 rounded-md cursor-pointer text-white"
         onClick={handleLogin}
         disabled={isLoading}
       >
         Login via Facebook
-      </button>
+      </button> */}
+        <LoginButton scope="public_profile,email" onCompleted={handleResponse}>
+        <span>Login with Facebook</span>
+      </LoginButton>
     </div>
   );
 }
