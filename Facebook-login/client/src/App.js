68209ab10b7f47bbac8a6eb55be879dc063419b6
arjunsen1999 <arjunsen717@gmail.com>
2023-10-17 12:44:44 +0530

    update

--- Facebook-login/client/src/App.js
+++ Facebook-login/client/src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useLogin } from "react-facebook";
+import "./App.css";
+
 
 function App() {
+  const { login, status, isLoading, error} = useLogin();
+  
+  async function handleLogin() {
+    try {
+      const response = await login({
+        scope: 'email',
+      });
+
+      console.log("response.status", response.status);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <div className="App">
-      
+      <button
+        className="bg-blue-900 py-2 px-4 rounded-md cursor-pointer text-white"
+        onClick={handleLogin}
+        disabled={isLoading}
+      >
+        Login via Facebook
+      </button>
     </div>
   );
 }
--- Facebook-login/client/src/index.js
+++ Facebook-login/client/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { FacebookProvider } from "react-facebook";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <FacebookProvider appId="669505088471550">
+      <App />
+    </FacebookProvider>
   </React.StrictMode>
 );
 
